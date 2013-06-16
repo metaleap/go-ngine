2eb7a7c414e5c9f7712d53da6f5d291b48b1945d
metaleap <philipp.schumann@gmail.com>
2013-06-16 23:25:24 +0200

    xmlx

--- _tools/buildtools/ng-gogl-imp-version-checker/main.go
+++ _tools/buildtools/ng-gogl-imp-version-checker/main.go
@@ -8,7 +8,7 @@ import (
 	"go/token"
 	"strings"
 
-	xmlx "github.com/goforks/go-pkg-xmlx"
+	xmlx "github.com/goforks/xmlx"
 
 	util "github.com/metaleap/go-util"
 	uio "github.com/metaleap/go-util/io"
